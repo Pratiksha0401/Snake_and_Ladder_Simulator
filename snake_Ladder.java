4a43b24ea5193b80e0a66372a9d50b43b655e63d
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-14 18:43:08 +0530

    Added: code of player rolls the die

--- snake_Ladder.java
+++ snake_Ladder.java
@@ -1,6 +1,12 @@
-class snake_Ladder {
-  public static void main(String[] args) {
-    int first_Player_positin=0;
-    System.out.println("Single Player's start position is at " + first_Player_positin);
-  }
+class snake_Ladder 
+{
+	public static void main(String[] args) 
+	{
+		int first_Player_position=0;
+		System.out.println("First Player at start position " + first_Player_position);
+		//rolls the die
+		int first_Player_roll=(int)Math.floor(Math.random()*6)+1;
+		first_Player_position=first_Player_roll+first_Player_position;
+		System.out.println("After rolls the die, first Player at position " + first_Player_position);
+	}
 }
