206acc1e2fffbff1c33eb7edf0f781b60f025e54
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-16 10:47:36 +0530

    Refactor: player should play again if gets ladder

--- snake_Ladder.java
+++ snake_Ladder.java
@@ -30,7 +30,7 @@ class snake_Ladder
       {
 			// the dice roll by First Player
 			int Player_roll=(int)Math.floor(Math.random()*6)+1;
-			System.out.println('\n'+" dice number"+ Player_roll);
+			System.out.println('\n'+" dice number  "+ Player_roll);
 			
 			//check for no play,Ladder or snake_Ladder_UC3
 			int check_Play=(int)Math.floor(Math.random()*10)%3;
@@ -38,14 +38,14 @@ class snake_Ladder
 				switch(check_Play)
 				{
 					case No_Play:
-						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						System.out.println(" Player  "+player_Number+" stays  at "+ Player_position);
 						break;
 					case Ladder:
 						Player_position+=Player_roll;
 						//ensure the player get exact 100 for winning
 						if(Player_position>100)
 							Player_position-=Player_roll;
-						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						System.out.println(" Player "+player_Number+" gets ladder and now at "+ Player_position);
 						player(player_Number,Player_position);
 						break;
 					case Snake:
@@ -53,7 +53,7 @@ class snake_Ladder
 						 //ensure the position must not below 0  
 						if(Player_position<0)
 							Player_position=0;
-						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						System.out.println(" Player "+player_Number+" gets snake and now at "+ Player_position);
 						break;
 				}
         return Player_position;
