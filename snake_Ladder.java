19e5195137ebb68bab0dcd27af51a751456f88fd
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-16 08:28:32 +0530

    Refactor: player should play again if gets ladder

--- snake_Ladder.java
+++ snake_Ladder.java
@@ -3,79 +3,59 @@ class snake_Ladder
 	public static final int No_Play=0;
 	public static final int Ladder=1;
 	public static final int Snake=2;
-	public static final int first_Player=0;
-	public static final int second_Player=1;
 
 	public static void main(String[] args) 
 	{
 		int first_Player_position=0;
 		int second_Player_position=0;
-
-		System.out.println("Player at start position " + first_Player_position);
-		
+		int player_Number;
 		//repeat till get winner
 		while(first_Player_position<100 &&  second_Player_position<100)
 		{
-			//roll the dice
-			int Player_roll=(int)Math.floor(Math.random()*6)+1;
+      //first player playing
+      first_Player_position=player(1,first_Player_position);
+     
+      //second player playing
+      second_Player_position=player(2,second_Player_position);
+    }
 
-			//which will play first or second
-			int player_Play=(int)Math.floor(Math.random()*2);
-			
-			if(player_Play==first_Player)
-			{
-				//check for no play,Ladder or snake_Ladder_UC3
-				int check_Play=(int)Math.floor(Math.random()*10)%3;
-			
-				switch(check_Play)
-				{
-					case No_Play:
-						System.out.println("First Player at "+first_Player_position);
-						break;
-					case Ladder:
-						first_Player_position+=Player_roll;
-						//ensure the player get exact 100 for winning
-						if(first_Player_position>100)
-							first_Player_position-=Player_roll;
-						System.out.println("First Player at "+first_Player_position);
-						break;
-					case Snake:
-						first_Player_position-=Player_roll;
-						if(first_Player_position<0)
-							first_Player_position=0;
-						System.out.println("First Player at "+first_Player_position);
-						break;
-				}
-			}
-			else
-			{
-				//check for no play,Ladder or snake_Ladder_UC3
-				int check_Play=(int)Math.floor(Math.random()*10)%3;
-				
-				switch(check_Play)
-				{
-					case No_Play:
-						System.out.println("Second Player at "+second_Player_position);
-					break;
-					case Ladder:
-						second_Player_position+=Player_roll;
-						//ensure the player get exact 100 for winning
-						if(second_Player_position>100)
-							second_Player_position-=Player_roll;
-						System.out.println("Second Player at "+second_Player_position);
-						break;
-					case Snake:
-						second_Player_position-=Player_roll;
-						if(second_Player_position<0)
-							second_Player_position=0;
-						System.out.println("Second Player at "+second_Player_position);
-						break;
-					}
-				}
-		}
+    //to get winner
 		if(first_Player_position==100)
 			System.out.println("First Player is Winner");
 		else
 			System.out.println("Second Player is Winner");
+
 	}
+   static int player(int player_Number,int Player_position)
+      {
+			// the dice roll by First Player
+			int Player_roll=(int)Math.floor(Math.random()*6)+1;
+			System.out.println('\n'+" dice number"+ Player_roll);
+			
+			//check for no play,Ladder or snake_Ladder_UC3
+			int check_Play=(int)Math.floor(Math.random()*10)%3;
+			
+				switch(check_Play)
+				{
+					case No_Play:
+						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						break;
+					case Ladder:
+						Player_position+=Player_roll;
+						//ensure the player get exact 100 for winning
+						if(Player_position>100)
+							Player_position-=Player_roll;
+						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						player(player_Number,Player_position);
+						break;
+					case Snake:
+						Player_position-=Player_roll;
+						 //ensure the position must not below 0  
+						if(Player_position<0)
+							Player_position=0;
+						System.out.println(" Player "+player_Number+" at "+ Player_position);
+						break;
+				}
+        return Player_position;
+      } 
 }
